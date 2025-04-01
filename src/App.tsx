dbedb4727cd415e810b9ab939e378bb5d87a1f47
Hyuk <poiu03290@naver.com>
2025-04-01 11:24:00 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+import "./App.css";
+import { MonthCalendar, WeekCalendar } from "react-custom-calendars";
+
+function App() {
+  const { weekDates, monthlyWeekGrid, currentDate } = WeekCalendar({
+    type: "dd",
+  });
+  const { monthCalendarData } = MonthCalendar({ type: "dd" });
+
+  return (
+    <>
+      <div>
+        <p>currentDate: {currentDate}</p>
+        <div>
+          <p>weekDates</p>
+          <ul>
+            {weekDates.map((date) => (
+              <li>{date}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>monthlyWeekGrid</p>
+          <ul>
+            {monthlyWeekGrid.map((weeks, weekIndex) => (
+              <li
+                key={weekIndex}
+                style={{
+                  listStyle: "none",
+                  display: "flex",
+                  gap: "10px",
+                }}
+              >
+                {weeks.map((date, dateIndex) => (
+                  <p key={dateIndex} style={{ width: "20px" }}>
+                    {date}
+                  </p>
+                ))}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <p>monthCalendarData</p>
+          <ul>
+            {monthCalendarData.map((weeks, weekIndex) => (
+              <li
+                key={weekIndex}
+                style={{
+                  listStyle: "none",
+                  display: "flex",
+                  gap: "10px",
+                }}
+              >
+                {weeks.map((date, dateIndex) => (
+                  <p key={dateIndex} style={{ width: "20px" }}>
+                    {date}
+                  </p>
+                ))}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
